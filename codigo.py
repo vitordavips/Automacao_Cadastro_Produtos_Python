924e3f314a2990a1826ee035da1a74f8dc2fbdaf
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-07-26 17:22:38 -0400

    fazendo os comandos para fazer o login

--- codigo.py
+++ codigo.py
@@ -1,13 +1,31 @@
+# Passo 1: importar as bibliotecas
 import pandas as pd
 import time
 import pyautogui
 
 # https://dlp.hashtagtreinamentos.com/python/intensivao/login
 
-pyautogui.PAUSE = 0.3
+
+pyautogui.PAUSE = 0.5
 
 # abrindo o navegador
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
+time.sleep(3)
+pyautogui.click(x=287, y=646, button='left')
 
+
+# entrar no link
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+time.sleep(3)
+
+# Passo 2: Fazer login
+#Selecionar o campo email
+pyautogui.click(x=594, y=364)
+# escrever o seu email
+pyautogui.write("vitordavidps@gmail.com")
+pyautogui.press("tab") # passando o próximo campo
+pyautogui.write("sua senha")
+pyautogui.click(x=679, y=468) # clique no botão de login
