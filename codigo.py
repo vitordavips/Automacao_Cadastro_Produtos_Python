0000706cb56105da6bfa2d6ed772fe9e746d09a3
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-07-26 16:25:40 -0400

    iniciando o projeto novamente

---
+++ codigo.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import time
+import pyautogui
+
+# https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
+pyautogui.PAUSE = 0.3
+
+# abrindo o navegador
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
