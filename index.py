0aee362e1e1348cab634d6b131cb85ccbd524290
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-07-26 16:08:07 -0400

    colocando os import

--- index.py
+++ index.py
@@ -1,37 +1,10 @@
+import pandas as pd
+import pyautogui
+import time
 
+# https://dlp.hashtagtreinamentos.com/python/intensivao/login
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# importar a base de produtos
+tabela = pd.read_csv("produtos.csv")
+print(tabela)
 
