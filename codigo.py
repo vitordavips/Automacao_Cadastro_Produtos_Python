aa6b5cbdc924895d8821771e5b4e590de302b353
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-07-27 16:26:02 -0400

    fazendo ele cadastrar os produtos

--- codigo.py
+++ codigo.py
@@ -28,20 +28,41 @@ pyautogui.click(x=594, y=364)
 pyautogui.write("vitordavidps@gmail.com")
 pyautogui.press("tab") # passando o próximo campo
 pyautogui.write("sua senha")
-pyautogui.click(x=679, y=468) # clique no botão de login
-time.sleep(3)
+pyautogui.click(x=693, y=532) # clique no botão de login
+time.sleep(2)
 
-# Passo 3: Importar a base de produtos pra cadastrar
+# Passo 3: Importar a vitordavidps@gmail.com    sua senha de produtos pra cadastrar
 tabela = pd.read_csv("produtos.csv")
 print(tabela)
 
 # Passo 4: Cadastrar um produto
-linha = 0
-# clicar no campo de código
-pyautogui.click(x=728, y=249)
-# pegar da tabela o valor do campo que a gente quer preencher
-codigo = "MOLO000251"
-# preencher o campo
+for linha in tabela.index:
+    # clicar no campo de código
+    pyautogui.click(x=728, y=249)
+    # pegar da tabela o valor do campo que a gente quer preencher
+    codigo = tabela.loc[linha, "codigo"]
+    # preencher o campo
+    pyautogui.write(str(codigo))
+    # Passar para o próximo campo
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna(obs): # O .insna() verificar se o campo está vazio.
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("enter") # cadastra o produto (botao enviar)
+    # dar um sroll pra cima
+    pyautogui.scroll(500)
 
 # Passo 5: Repetir o processo de cadastro até o fim
 
--- pegar_posicao.py
+++ pegar_posicao.py
@@ -4,4 +4,4 @@ import pyautogui
 time.sleep(5)
 print(pyautogui.position())
 
-pyautogui.scroll(200)
+pyautogui.scroll(400)
